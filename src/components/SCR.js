7d43a04e27031c546a1c9134d12e6d0e8dffc6e1
Nathan Broaddus <nathanbroaddus@gmail.com>
2017-03-11 00:52:31 -0500

    [Tarot] refactoring, initial build out for single card draw

--- src/components/SCR.js
+++ src/components/SCR.js
@@ -10,6 +10,7 @@ class SCR extends Component {
          this.state = {
              name: 'Card',
              src: null,
+             random_card_flipped: null,
              random_card_name: null,
              random_card_description: null,
              random_card_upright: null,
@@ -17,16 +18,20 @@ class SCR extends Component {
          };
    }
    componentDidMount() {
-       console.log('Cards', this.props.Cards);
+       // console.log('Cards', this.props.Cards);
+
       }
       
        getRandomCard() {
             console.group('get random card');
+            let flipped =   (Math.floor(Math.random() * 2) == 0) ? true : false;
             let self = this;
             let cards = this.props.Cards;
             let randNum = Math.floor(Math.random() * 77) + 1;
-            let randCard = cards[15];
+            let randCard = cards[randNum];
             let randName = randCard.name;
+            let randKeysUp  = randCard.keywords.upright;
+            let randKeysRev  = randCard.keywords.reversed;
             let src = randCard.src;
             let randDes = randCard.description.basic;
             let randUpright = randCard.description.upright;
@@ -35,25 +40,33 @@ class SCR extends Component {
             console.log('card drawn',randCard);
             $('.description').innerHTML = randDes;
             
-            reading.fadeOut('slow', function(){
+            reading.fadeOut('fast', function(){
                self.setState(
                   {
+                     random_card_flipped: flipped,
                      random_card_name: randName,
+                     random_card_keys_up: randKeysUp,
+                     random_card_keys_rev: randKeysRev,
                      random_card_description: randDes,
                      random_card_upright: randUpright,
                      random_card_reversed: randReversed,
                      random_card_src: src
 
                   }, function(){
-                  console.log('random state:', this.state);
-                  console.log( $('.description')[0].innerHTML);
                   this.replace_break();
+
+                  if (this.state.random_card_flipped){
+                     $('.card_image').addClass('card_image_reversed');
+                  } else {
+                     $('.card_image').removeClass('card_image_reversed');
+                  }
+
+                  reading.fadeIn('fast');
                });
             });
             console.groupEnd();
-            reading.fadeIn('slow');
-
        }
+       
        replace_break(){
          console.log('replace break');
          let paras = document.getElementsByTagName('p');
@@ -65,21 +78,42 @@ class SCR extends Component {
        }
 
        render(){
+         if (!this.state.random_card_flipped){
             return (
                 <div className="main-content SCR">    
                      <button onClick={ this.getRandomCard}>Draw A Single Card</button>
                      <div style={{display: 'none'}} className="single_card_reading">
-                        <h3>{this.state.random_card_name}</h3><img style={{float: 'right'}} src={'assets/tarot/'+this.state.random_card_src} alt=""/>
+                        <h3>{this.state.random_card_name}</h3><img className="card_image" style={{float: 'right'}} src={'assets/tarot/'+this.state.random_card_src} alt=""/>
+                        <h4>Keywords: Upright</h4>
+                        <p>{this.state.random_card_keys_up}</p>
+                        <h3>Summary</h3>
                         <p className="description">{this.state.random_card_description}</p>
                         <h3>Upright</h3>
                         <p className="upright_description">{this.state.random_card_upright}</p>
+                     </div>
+                     
+                </div>
+            )
+         } else {
+            return (
+                <div className="main-content SCR">    
+                     <button onClick={ this.getRandomCard}>Draw A Single Card</button>
+                     <div style={{display: 'none'}} className="single_card_reading">
+                        <h3>{this.state.random_card_name +' Reversed'}</h3><img className="card_image" style={{float: 'right'}} src={'assets/tarot/'+this.state.random_card_src} alt=""/>
+                        <h4>Keywords: </h4>
+                        <p>{this.state.random_card_keys_rev}</p>
+                        <h3>Summary</h3>
+                        <p className="description">{this.state.random_card_description}</p>
                         <h3>Reversed</h3>
                         <p className="upright_description">{this.state.random_card_reversed}</p>
                      </div>
                      
                 </div>
             )
+         }
+            
        }
+         
 }
 
 // PROPS 
@@ -93,7 +127,6 @@ SCR.propTypes = {
     name: React.PropTypes.string.isRequired,
     kind: React.PropTypes.string
 }
-
 
 
 export default SCR;
