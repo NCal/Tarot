bd1f7075eb877c46cf8d1ca37e8be2edee86513b
Nathan Broaddus <nathanbroaddus@gmail.com>
2017-06-25 16:14:43 -0400

    [tarot] loader

--- src/components/SCR.js
+++ src/components/SCR.js
@@ -81,24 +81,9 @@ class SCR extends Component {
    if (this.state.loading){
       return (
       <div className="main-content SCR">   
-         <div>
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-              <defs>
-                <filter id="goo">
-                  <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 35 -10" result="goo" />
-                  <feBlend in="SourceGraphic" in2="goo" operator="atop" />
-                </filter>
-              </defs>
-            </svg>
-            <div className="loader">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-         </div> 
+          <div className="main-content ">   
+          <img className="pentacle" src="https://upload.wikimedia.org/wikipedia/commons/9/97/Pentacle_2.svg"/>
+        </div>
       </div>
       )
    }
